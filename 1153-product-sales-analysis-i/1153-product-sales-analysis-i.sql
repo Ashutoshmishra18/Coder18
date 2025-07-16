dbebcd6c0bc930bb8132de7a15ee8ba29f8634e7
Ashutosh Mishra <121113509+Ashutoshmishra18@users.noreply.github.com>
2025-07-16 23:14:25 +0530

    Time: 1482 ms (14.59%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,18 +1,4 @@
-#Approach 3-CTE(Common Table Expression)
-WITH SaleDetails AS (
-    SELECT 
-        s.sale_id,
-        p.product_name,
-        s.year,
-        s.price
-    FROM 
-        Sales s
-    JOIN 
-        Product p ON s.product_id = p.product_id
-)
-SELECT 
-    product_name,
-    year,
-    price
-FROM 
-    SaleDetails;
+# Write your MySQL query statement below
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+JOIN Product p on s.product_id= p.product_id;
